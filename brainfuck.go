ce3ff423be39c32448d7f2a750bc37b873216381
Lauri Hakko <lauri.hakko@gmail.com>
2011-12-16 13:16:20 +0200

    ran through gofmt

--- brainfuck.go
+++ brainfuck.go
@@ -17,94 +17,93 @@ package main
  */
 
 import (
-    "fmt"
-    "flag"
-    "os"
-    "io/ioutil"
-    )
-
+	"fmt"
+	"flag"
+	"os"
+	"io/ioutil"
+)
 
 func interpreter(code []byte) {
-    tape := make([]byte, 10000)
-    pointer := 5000
-    codeptr := 0
-    one_byte := make([]byte, 1, 1)
-    for code[codeptr] != 0 {
-        switch code[codeptr] {
-            case '>':
-                pointer++
-                if pointer >= len(tape) {
-                	pointer = 0
+	tape := make([]byte, 10000)
+	pointer := 5000
+	codeptr := 0
+	one_byte := make([]byte, 1, 1)
+	for code[codeptr] != 0 {
+		switch code[codeptr] {
+		case '>':
+			pointer++
+			if pointer >= len(tape) {
+				pointer = 0
+			}
+		case '<':
+			pointer--
+			if pointer < 0 {
+				pointer = len(tape) - 1
+			}
+		case '+':
+			tape[pointer]++
+		case '-':
+			tape[pointer]--
+		case '.':
+			fmt.Printf("%c", tape[pointer])
+		case ',':
+			os.Stdin.Read(one_byte)
+			tape[pointer] = one_byte[0]
+		case '[':
+			if tape[pointer] == 0 {
+				counter := 1
+				for !(code[codeptr] == ']' && counter == 0) {
+					codeptr++
+					if code[codeptr] == '[' {
+						counter++
+					} else if code[codeptr] == ']' {
+						counter--
+					}
 				}
-            case '<':
-                pointer--
-                if pointer < 0 {
-                	pointer = len(tape) - 1
+			}
+		case ']':
+			if tape[pointer] != 0 {
+				counter := 1
+				for !(code[codeptr] == '[' && counter == 0) {
+					codeptr--
+					if code[codeptr] == ']' {
+						counter++
+					} else if code[codeptr] == '[' {
+						counter--
+					}
 				}
-            case '+':
-                tape[pointer]++
-            case '-':
-                tape[pointer]--
-            case '.':
-                fmt.Printf("%c", tape[pointer])
-            case ',':
-                os.Stdin.Read(one_byte)
-                tape[pointer] = one_byte[0]
-            case '[':
-                if tape[pointer] == 0 {
-                    counter := 1
-                    for !(code[codeptr] == ']' && counter == 0) {
-                        codeptr++
-                        if code[codeptr] == '[' {
-                            counter++
-                        } else if code[codeptr] == ']' {
-                            counter--
-                        }
-                    }
-                }
-            case ']':
-                if tape[pointer] != 0 {
-                    counter := 1
-                    for !(code[codeptr] == '[' && counter == 0) {
-                        codeptr--
-                        if code[codeptr] == ']' {
-                            counter++
-                        } else if code[codeptr] == '[' {
-                            counter--
-                        }
-                    }
-                }
-        }
-        codeptr++
-    }
+			}
+		}
+		codeptr++
+	}
 }
 
 func is_bf_char(item byte) bool {
-    chars := [8]byte{'>', '<', '+', '-', '.', ',', '[', ']'}
-    var contained bool
-    for _, v := range chars {
-        if item == v {
-            contained = true
-            break
-        }
-    }
-    return contained
+	chars := [8]byte{'>', '<', '+', '-', '.', ',', '[', ']'}
+	var contained bool
+	for _, v := range chars {
+		if item == v {
+			contained = true
+			break
+		}
+	}
+	return contained
 }
 
 func main() {
-    filename := flag.String("src", "", "Specify the codefile to use.")
-    flag.Parse()
-    source, err := ioutil.ReadFile(*filename)
-    if err != nil {
-    	fmt.Print("File could not be read.")
+	filename := flag.String("src", "", "Specify the codefile to use.")
+	flag.Parse()
+	source, err := ioutil.ReadFile(*filename)
+	if err != nil {
+		fmt.Print("File could not be read.")
 
-    }
-    code := make([]byte, 0)
-    for _, v := range source {
-        if is_bf_char(v) {
-            code = append(code, v)
-        }
-    }
-    code = append(code, 0)
-    interpreter(code)
+	}
+	code := make([]byte, 0)
+	for _, v := range source {
+		if is_bf_char(v) {
+			code = append(code, v)
+		}
+	}
+	code = append(code, 0)
+	interpreter(code)
 }
